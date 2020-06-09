d313202902508ded0c58fa63c05687c0e7ab82f1
Thymon <t.bogels@thingks.nl>
2020-06-09 11:08:58 +0200

    add test react project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './Navbar';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Navbar/>
+    </div>
+    
+  );
+}
+
+export default App;
